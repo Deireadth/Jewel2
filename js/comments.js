ee1b7afc05bc3eeea3d40b65355aaeb0337618e4
Lukasz Poniechowski <lukaszponiechowski@gmail.com>
2017-02-16 01:39:02 +0100

    update

---
+++ js/comments.js
@@ -0,0 +1,12 @@
+﻿if (window.innerWidth<480) setTimeout("zmienKomentarz()", 5000);
+var widoczny = 1
+
+function zmienKomentarz() {
+    $("#comment" + widoczny).fadeOut(500)
+    $("#comment" + widoczny).addClass("mobileHidden")
+    widoczny++
+    if (widoczny > 6) widoczny = 1
+    $("#comment" + widoczny).removeClass("mobileHidden")
+    $("#comment" + widoczny).fadeIn(450)
+    setTimeout("zmienKomentarz()", 5000);
+}
